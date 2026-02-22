ab51cda9942df0373618a4f6d52e896b610c2947
Alchemist <alchemist@alchemizedev.local>
2026-02-22 14:11:02 -0500

    feat: Add ComponentTreeWidget field widget for component_tree fields
    Removes no_ui: TRUE from ComponentTreeItem field type and adds
    a default_widget pointing to the new ComponentTreeWidget.
    
    The widget renders an "Edit in Canvas" link that takes the user
    to the entity's Canvas layout editor. It also implements a no-op
    extractFormValues() to prevent the standard form submission from
    overwriting the component tree data that Canvas manages via API.

--- src/Plugin/Field/FieldType/ComponentTreeItem.php
+++ src/Plugin/Field/FieldType/ComponentTreeItem.php
@@ -48,9 +48,7 @@ use Symfony\Component\Validator\ConstraintViolationList;
   label: new TranslatableMarkup("Drupal Canvas"),
   description: new TranslatableMarkup("Field to use Drupal Canvas for presenting these entities"),
   default_formatter: "canvas_naive_render_sdc_tree",
-  // @todo Revisit this prior to 1.0.
-  // @see https://www.drupal.org/project/canvas/issues/3497926
-  no_ui: TRUE,
+  default_widget: "canvas_component_tree_widget",
   list_class: ComponentTreeItemList::class,
   // This only makes sense in a multi-value context: each item is a node in the
   // component tree.
